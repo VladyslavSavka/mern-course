bca175c6cad6e2e8f2fa9aa14235ffc922e89559
Vlad Savka <vladyslav.savka@wetelo.com>
2021-01-06 15:48:37 +0200

    initial commit

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import { useHttp } from '../hooks/http.hook';
+import { useMessage } from '../hooks/message.hook';
+
+export const AuthPage = () => {
+    const message = useMessage();
+    const {loading, request, error, clearError} = useHttp()
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+
+    useEffect(() => {
+        message(error);
+        clearError();
+    }, [error, message, clearError]);
+
+    const changehandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            message(data.message)
+        } catch (e) {}
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            message(data.message)
+        } catch (e) {}
+    }
+
+    return (
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>URL Shortener</h1>
+                <div className="row">
+                    <div className="col s12 m6">
+                    <div className="card blue darken-1">
+                        <div className="card-content white-text">
+                        <span className="card-title"> Авторизация </span>
+                        <div>
+                            <div className="input-field">
+                                <input 
+                                    placeholder="Введите email"
+                                    id='email'
+                                    type="text"
+                                    name='email'
+                                    className="yellow-input"
+                                    onChange={changehandler}
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input 
+                                    placeholder="Введите пароль"
+                                    id='password'
+                                    type="password"
+                                    name="password"
+                                    className="yellow-input"
+                                    onChange={changehandler}
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="card-action">
+                            <button 
+                                className='btn yellow darken-4'
+                                style={{margin: 10}}
+                                onClick={loginHandler}
+                                disabled={loading}
+                            >
+                                    Ввойти
+                            </button>
+                            <button 
+                            className='btn grey lighten-1 black-text'
+                            onClick={registerHandler}
+                            disabled={loading}
+                            >
+                                Регистрация
+                            </button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
